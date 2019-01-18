73680730e1d953c8d7be6f9c596d530c583d378a
Jānis Veinbergs <JVeinbergs@deac.eu>
2019-01-18 18:01:08 +0200

    Introduce new options: HrefHandling (Smart) and WhitelistUriSchemes. Gives more control over when convert to markdown, when simplify output and when remove output. See README.md for more info. v1.0.3

--- src/ReverseMarkdown/Converters/Img.cs
+++ src/ReverseMarkdown/Converters/Img.cs
@@ -1,7 +1,7 @@
 ﻿using HtmlAgilityPack;
+using System;
 
-namespace ReverseMarkdown.Converters
-{
+namespace ReverseMarkdown.Converters {
     public class Img : ConverterBase
     {
         public Img(Converter converter) : base(converter)
@@ -11,12 +11,22 @@ namespace ReverseMarkdown.Converters
 
 		public override string Convert(HtmlNode node)
 		{
-            if (Converter.Config.BarePlaintext)
-                return "";
+            if (Converter.Config.BarePlaintext) { return ""; }
 			string alt = node.GetAttributeValue("alt", string.Empty);
 			string src = node.GetAttributeValue("src", string.Empty);
-			string title = this.ExtractTitle(node);
 
+            var scheme = "";
+            bool isRelativeUrl = false;
+            try {
+                scheme = (new Uri(src, UriKind.Absolute)).Scheme;
+            }
+            catch {
+                isRelativeUrl = true;
+            }
+
+            if (!isRelativeUrl && !Converter.Config.IsSchemeAllowed(scheme)) { return ""; }
+
+            string title = this.ExtractTitle(node);
             title = title.Length > 0 ? $" \"{title}\"" : "";
 
             return $"![{alt}]({src}{title})";
